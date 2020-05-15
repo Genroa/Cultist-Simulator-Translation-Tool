4913c264c77aedc827259a97c8a43f9b7c63cc6f
Genroa <genroa@hotmail.fr>
2020-05-15 22:02:41 +0200

    Initial files

---
+++ precache-manifest.7c4fb177d1c866a960948a7b6d4f1b60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4493a18f5b42d3ce8484",
+    "url": "/css/app.e7196e0f.css"
+  },
+  {
+    "revision": "bcb4cdb094e7c6b1f16b",
+    "url": "/css/chunk-vendors.b27e6a3f.css"
+  },
+  {
+    "revision": "16767b8d873b259b409bbf4b2f49c015",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4493a18f5b42d3ce8484",
+    "url": "/js/app.7dcfa504.js"
+  },
+  {
+    "revision": "bcb4cdb094e7c6b1f16b",
+    "url": "/js/chunk-vendors.bd54ef50.js"
+  },
+  {
+    "revision": "1091abf28e885dad2be46ba6468a5a7f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
